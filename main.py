d1c8157479d696cfaf29baf448d3ee05d28cac1e
Pavel <2793553@gmail.com>
2023-10-16 17:15:22 +0300

    task04 done

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Passcard
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    [print(f'{passcard.owner_name}\n'
-           f'{passcard.passcode}\n'
-           f'{passcard.created_at}\n'
-           f'{passcard.is_active}\n') for passcard  in Passcard.objects.all()]
+    print('Количество пропусков:', Passcard.objects.count())
+    print('Активных пропусков:', len(Passcard.objects.filter(is_active=True)))
+
